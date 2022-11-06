e91fdc9f20ae3a83c24c821c901f3f257757c923
นายภาณุพงษ์ สุขส่ง <59049140+Panupong62@users.noreply.github.com>
2022-11-07 00:27:44 +0700

    kuOhm

---
+++ profile.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>profile</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/templatemo-blue.css">
+
+</head>
+<body data-spy="scroll" data-target=".navbar-collapse">
+
+<!-- preloader section -->
+<div class="preloader">
+	<div class="sk-spinner sk-spinner-wordpress">
+       <span class="sk-inner-circle"></span>
+     </div>
+</div>
+
+<!-- header section -->
+<header>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12" >
+				<img src="images/po03.png" class="img-responsive img-circle tm-border" alt="templatemo easy profile">
+				<hr>
+				<h1 class="tm-title bold shadow">Panupong Suksong</h1>
+				<h1 class="white bold shadow">โอม</h1>
+			</div>
+		</div>
+	</div>
+</header>
+
+<!-- about and skills section -->
+<section class="container">
+	<div class="row">
+		<div class="col-md-6 col-sm-12">
+			<div class="about">
+				<h3 class="accent">Panupong Suksong</h3>
+				<h4>นักศึกษา วิทคอมปี4</h4>
+				<p>ชื่อ ภาณุพงษ์ สุขส่ง  ชื่อเล่น โอม นักศึกษา วิทคอมปี4  <br> ถึงผลการเรียนจะไม่ดีเเต่ก็อยากจบ 4 ปีนะครับจารย์ </p>
+			</div>
+		</div>
+		<div class="col-md-6 col-sm-12">
+			<div class="skills">
+				<h2 class="white">Skills</h2>
+				<strong>Programming</strong>
+				<span class="pull-right">60%</span>
+					<div class="progress">
+						<div class="progress-bar progress-bar-primary" role="progressbar" 
+                        aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
+					</div>
+				<strong>Database</strong>
+				<span class="pull-right">50%</span>
+					<div class="progress">
+						<div class="progress-bar progress-bar-primary" role="progressbar" 
+                        aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;"></div>
+					</div>
+                    <strong>Web Technology</strong>
+				<span class="pull-right">45%</span>
+					<div class="progress">
+						<div class="progress-bar progress-bar-primary" role="progressbar" 
+                        aria-valuenow="45" aria-valuemin="0" aria-valuemax="100" style="width: 45%;"></div>
+					</div>
+                    <strong>Analysis and Design</strong>
+				<span class="pull-right">40%</span>
+					<div class="progress">
+						<div class="progress-bar progress-bar-primary" role="progressbar" 
+                        aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;"></div>
+					</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- education and languages -->
+<section class="container">
+	<div class="row">
+		<div class="col-md-8 col-sm-12">
+			<div class="education">
+				<h2 class="white">การศึกษาระดับมัธยม</h2>
+					<div class="education-content">
+						<h4 class="education-title accent">จบมัธยมที้</h4>
+							<div class="education-school">
+								<h6>โรงเรียนเรณูนครวิทยานุกูล</h6></h8></span>
+                                <h6 class="education-title accent">เกรดเฉลี่ย</h6>
+                                <h8>GPA 2.5</h8>
+							</div>
+                           
+					</div>
+			</div>
+		</div>
+		<div class="col-md-4 col-sm-12">
+			<div class="languages">
+				<h2>ภาษา</h2>
+					<ul>
+						<li>Thai</li>
+					</ul>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- contact and experience -->
+<section class="container">
+	<div class="row">
+		<div class="col-md-4 col-sm-12">
+			<div class="contact">
+				<h2>ติดต่อฉัน</h2>
+					<p><i class="fa fa-map-marker"></i> 17/1 หมู่3 ต.นางาม อ.เรณู จ.นครพนม </p>
+					<p><i class="fa fa-phone"></i> 061-095-3083</p>
+					<p><i class="fa fa-envelope"></i> panupongSuksong@kkumail.com</p>
+					
+			</div>
+		</div>
+		<div class="col-md-8 col-sm-12">
+			<div class="experience">
+				<h2 class="white">การศึกษาระดับปริญญาตรี</h2>
+					<div class="experience-content">
+						<h4 class="experience-title accent">ปัจจุบันศึกษาระดับปริญญาตรีที่</h4>
+						<h6>มหาวิทยาลัยขอนแก่น</h6><span></span>
+                        <h6 class="education-title accent">เกรดเฉลี่ย</h6>
+                        <h8>GPA 2.58</h8>
+					</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- footer section -->
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12">
+                <hr>
+				<p>ติดตามฉันได้ที่ &copy; My Profile</p>
+				<ul class="social-icons">
+					<li><a href="https://www.facebook.com/panupong.suksong/" class="fa fa-facebook"></a></li>
+					<li><a href="https://www.instagram.com/" class="fa fa-instagram"></a></li>
+					<li><a href="#" class="fa fa-github"></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<!-- javascript js -->	
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>	
+<script src="js/jquery.backstretch.min.js"></script>
+<script src="js/custom.js"></script>
+    
+</body>
+</html>
